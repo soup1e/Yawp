94bdc7f157a7e34baec2115a73121684c0f13dc2
Sam Adams <sam.adams7785@gmail.com>
2022-11-28 15:13:40 -0800

    GET resturants test passing

--- __tests__/restaurant.test.js
+++ __tests__/restaurant.test.js
@@ -12,11 +12,43 @@ describe('blog routes', () => {
   });
 
   it('GET api/v1/restaurant should return list of restaurant', async () => {
-    const res = await request(app).get('/api/v1/restaurant/1');
+    const res = await request(app).get('/api/v1/restaurants');
     expect(res.status).toBe(200);
     expect(res.body).toMatchInlineSnapshot(`
-    Array [
-      Object {},
-    ]`);
+      Array [
+        Object {
+          "cost": 1,
+          "cuisine": "American",
+          "id": "1",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+          "name": "Pip's Original",
+          "website": "http://www.PipsOriginal.com",
+        },
+        Object {
+          "cost": 3,
+          "cuisine": "Italian",
+          "id": "2",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
+          "name": "Mucca Osteria",
+          "website": "http://www.muccaosteria.com",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "Mediterranean",
+          "id": "3",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/f2/e5/0c/dinner.jpg",
+          "name": "Mediterranean Exploration Company",
+          "website": "http://www.mediterraneanexplorationcompany.com/",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "American",
+          "id": "4",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/0d/d6/a1/06/chocolate-gooey-brownie.jpg",
+          "name": "Salt & Straw",
+          "website": "https://saltandstraw.com/pages/nw-23",
+        },
+      ]
+    `);
   });
 });
