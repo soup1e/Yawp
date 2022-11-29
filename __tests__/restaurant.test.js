a2bf0e6ddc643842e4e3727dd7ab72dc3b036428
Sam Adams <sam.adams7785@gmail.com>
2022-11-28 16:22:59 -0800

    GET single restaurant and nested reviews Passing

--- __tests__/restaurant.test.js
+++ __tests__/restaurant.test.js
@@ -55,6 +55,38 @@ describe('blog routes', () => {
   it('GET api/v1/restaurant/1 should return a single restaurant with nested comments', async () => {
     const res = await request(app).get('/api/v1/restaurants/1');
     expect(res.status).toBe(200);
-    expect(res.body).toMatchInlineSnapshot();
+    expect(res.body).toMatchInlineSnapshot(`
+      Object {
+        "comments": Array [
+          Object {
+            "detail": "Best restaurant ever!",
+            "id": "1",
+            "restaurant_id": "1",
+            "stars": 5,
+            "user_id": "1",
+          },
+          Object {
+            "detail": "Terrible service :(",
+            "id": "2",
+            "restaurant_id": "1",
+            "stars": 1,
+            "user_id": "2",
+          },
+          Object {
+            "detail": "It was fine.",
+            "id": "3",
+            "restaurant_id": "1",
+            "stars": 4,
+            "user_id": "3",
+          },
+        ],
+        "cost": 1,
+        "cuisine": "American",
+        "id": "1",
+        "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+        "name": "Pip's Original",
+        "website": "http://www.PipsOriginal.com",
+      }
+    `);
   });
 });
